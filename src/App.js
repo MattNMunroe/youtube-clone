7af34133fbf99220e31ba562f6ec48d0ec527c16
arupay <arupay11374@gmail.com>
2022-06-14 12:28:02 -0400

    fixed some errors

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import NavBar from './Components/NavBar'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
 import Search from "./Components/Search";
 
 class App extends React.Component {
-  constructor(){
-    super()
-      this.state = {
-        input: ''
-      }
-    }
-  
-handleOnChange = (event) => {
-this.setState({
-  input: event.target.value 
-})
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
 
-
-}
-  render () {
-  return (
-    <div className="App">
-      <Router>
-      <Routes>
-        <Route path="/" element={<NavBar/>}/>
-     </Routes>
-      <Search search={this.state.input} handleOnChange={this.handleOnChange}/>
-      </Router>
-    </div>
-  );
+  handleOnChange = (event) => {
+    this.setState({
+      input: event.target.value,
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<NavBar />} />
+        </Routes>
+        <Search
+          search={this.state.input}
+          handleOnChange={this.handleOnChange}
+        />
+      </div>
+    );
   }
 }
 
