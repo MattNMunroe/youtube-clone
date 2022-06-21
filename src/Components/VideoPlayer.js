77a82c12302369c3599c16415dfdf030099eb196
Matt M <Afrodesiacc@gmail.com>
2022-06-21 05:04:23 -0400

    Added padding to video player, changed fonts, and created a colored background for our button.

--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -2,6 +2,7 @@ import React from "react";
 import YouTube from "react-youtube";
 import { useParams } from "react-router-dom";
 import { useState } from "react";
+import "./VideoPlayer.css"
 
 const VideoPlayer = () => {
   let videoId = useParams().id;
