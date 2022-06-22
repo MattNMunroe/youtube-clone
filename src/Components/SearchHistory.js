4f3277c9fd204c5d36347aaa004dcf7cb546e5e3
KamNotCam <Kedari.a.matthews@gmail.com>
2022-06-21 21:41:06 -0400

    SearchHistory merge

---
+++ src/Components/SearchHistory.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+
+const SearchHistory = (props) => {
+
+    const [isShowing, setShowing] = useState(false)
+    
+
+    return (
+        <div>
+            {isShowing && props.searchHistory.map((history) => {return (<div>{history}</div>)})}
+        <button onClick={() => setShowing(!isShowing)}>{isShowing ? 'Close History' : 'Show History'}</button>
+        </div>
+    )
+}
+
+export default SearchHistory
